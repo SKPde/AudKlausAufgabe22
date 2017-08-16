d70d2a33320bb66cc6b2b4b062d8fe5f579d0475
s.kuebel <s.kuebel@otterbach.de>
2017-08-16 10:36:08 +0200

    Gibt nun gegebenenfalls aus, dass es sich um einen Rundungsfehler handelt

--- src/ReelleZahl.java
+++ src/ReelleZahl.java
@@ -1,30 +1,40 @@
 
-public class ReelleZahl extends Zahl implements Comparable{
-	
+public class ReelleZahl extends Zahl implements Comparable {
+
 	private double value; // Wert der reellen Zahl
-	
+
 	public ReelleZahl(double v) { // Konstruktor
-		
+		value = v;
 	}
 
 	@Override
-	public Zahl sub(Zahl zahl) { // Subtraktionsmethode
+	public ReelleZahl sub(Zahl zahl) { // Subtraktionsmethode
 		// TODO Auto-generated method stub
-		return null;
+		ReelleZahl temp = (ReelleZahl) zahl;
+		return new ReelleZahl(this.value - temp.value);
+	}
+
+	public boolean equals(Object zahl) {
+		ReelleZahl temp = (ReelleZahl) zahl;
+		return this.value == temp.value; // Vergleichsmethode
+
 	}
 	
-	public boolean equals(Object Zahl) {
-		
-		
-		
-		return false; // Vergleichsmethode
-
+	public double getValue() {
+		return value;
 	}
 
 	@Override
 	public int compareTo(Object o) { // Vergleichsmethode
 		// TODO Auto-generated method stub
-		return 0;
+		ReelleZahl temp = (ReelleZahl) o;
+		if(this.value > temp.value) {
+			return -1;
+		} else if (this.value < temp.value) {
+			return 1;
+		} else {
+			return 0;
+		}
 	}
 
 }
--- src/ReellerAlg.java
+++ src/ReellerAlg.java
@@ -3,7 +3,17 @@ public class ReellerAlg {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ReelleZahl x = new ReelleZahl(0.1);
+		ReelleZahl y = new ReelleZahl(10);
+		
+		while(y.compareTo(new ReelleZahl(0)) < 0) {
+			y = y.sub(x);
+			System.out.println("Durchlauf: " + y.getValue());
+		}
+		
+		if(! y.equals(new ReelleZahl(0))) {
+			System.out.println("Rundungsfehler");
+		}
 	}
 
 }
